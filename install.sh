e4febcc62566149fe55cd68245562f27d93f366e
Julien ROLLIN <julien.rollin@talan.com>
2019-03-08 13:51:02 +0100

    add installation steps

---
+++ install.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.0
+
+# The following steps are for bash. If you’re using something else, do the
+# equivalent for your shell.
+echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+
+## zsh
+echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
+echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
+
+## fish
+echo 'source ~/.asdf/asdf.fish' >> ~/.config/fish/config.fish
+mkdir -p ~/.config/fish/completions; and cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+
+## asdf plugins (not required)
+sudo apt install \
+  automake autoconf libreadline-dev \
+  libncurses-dev libssl-dev libyaml-dev \
+  libxslt-dev libffi-dev libtool unixodbc-dev
+  
