d54d54a07848c7ecaa4ed8ce0137b53bc4011f80
jrollin <1086565+jrollin@users.noreply.github.com>
2019-11-17 13:02:09 +0100

    bump hugo version

--- plugins/go.sh
+++ plugins/go.sh
@@ -7,5 +7,5 @@ asdf plugin-add hugo https://bitbucket.org/mgladdish/asdf-gohugo.git
 asdf install golang 1.13.4
 asdf global golang 1.13.4
 
-asdf install hugo 0.55.0
-asdf global hugo 0.55.0
+asdf install hugo 0.59.1
+asdf global hugo 0.59.1
